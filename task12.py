2e052860f2f79e3f14f563e3c61d18fe645f9d0e
Nodirbek <berdimurodovnodir89@gmail.com>
2025-12-07 15:01:40 +0500

    task12.py bajarildi

---
+++ task12.py
@@ -0,0 +1,7 @@
+password1=input('parpl kiriting :')
+password2=input('parolni takrorlang :')
+
+if password1 == password2 :
+    print('Parol qabul qilindi ')
+else :
+    print('Parollar mos emas ')
