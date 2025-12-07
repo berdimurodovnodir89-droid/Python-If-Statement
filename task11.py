0f33d28f95d15c48c5906c601ad81942b4ee57ed
Nodirbek <berdimurodovnodir89@gmail.com>
2025-12-07 15:00:36 +0500

    task11.py bajarildi

---
+++ task11.py
@@ -0,0 +1,26 @@
+season=int(input("Raqam kiriting :"))
+
+if season == 12 :
+    print('Qish')
+elif season == 1 :
+    print('Qish')
+elif season == 2 :
+    print('Qish')
+elif season == 3 :
+    print("Bahor")
+elif season == 4 :
+    print("Bahor")
+elif season == 5 :
+    print("Bahor")
+elif season == 6 :
+    print("yoz")
+elif season == 7 :
+    print('Yoz')
+elif season == 8 :
+    print("Yoz")
+elif season == 9 :
+    print('Kuz')
+elif season == 10 :
+    print('Kuz')
+elif season == 11 :
+    print('Kuz')
