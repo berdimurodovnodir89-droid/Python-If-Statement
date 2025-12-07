e918e15d0767518194237b75770632af2aa3581e
Nodirbek <berdimurodovnodir89@gmail.com>
2025-12-07 14:57:06 +0500

    task07.py

---
+++ task07.py
@@ -0,0 +1,8 @@
+email=input('Emailingizni kiriting :')
+
+if "@." in email :
+    print("email manzili togri")
+elif email == ' ' :
+    print('email manzili togri')
+else :
+    print('email manzili notogri')
