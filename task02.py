53fba037e7e0906d58d98905eff49b2022e0eaae
Nodirbek <berdimurodovnodir89@gmail.com>
2025-12-07 14:41:49 +0500

    task02.py

---
+++ task02.py
@@ -0,0 +1,11 @@
+num1=float(input('1-sonni kiriting :'))
+num2=float(input('2-sonni kiriting :'))
+
+if num1 > num2 :
+    print(num1)
+
+if num1 < num2 :
+    print(num2)
+
+if num1 == num2 :
+    print('teng')
